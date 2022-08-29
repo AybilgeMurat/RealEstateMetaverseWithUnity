e74336e4ab9a136e8139a8fe92ebb24bdf93e681
Ayşe Aybilge MURAT <58533563+AybilgeMurat@users.noreply.github.com>
2022-08-29 16:43:03 +0300

    Add files via upload

---
+++ Project/showHideInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class showHideInfo : MonoBehaviour
+{
+    public GameObject InfoObject;
+    private bool show = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowandHideInfo()
+    {
+        if (!show)
+        {
+            InfoObject.SetActive(true);
+            show = true;
+        }
+        else { InfoObject.SetActive(false);
+            show = false;
+        }
+
+    }
+
+}
